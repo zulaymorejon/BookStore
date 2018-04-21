b5cd5791719bf6f3c31042b6b598b255e7910604
zedih91lay@gmail.com <zedih91lay@gmail.com>
2018-04-21 16:23:49 -0500

    WebPack

---
+++ boostore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyAwtXOLgliHCDyZApW637vu6_x2YpJAez4",
+    authDomain: "bookstore-26c0b.firebaseapp.com",
+    databaseURL: "https://bookstore-26c0b.firebaseio.com",
+    projectId: "bookstore-26c0b",
+    storageBucket: "bookstore-26c0b.appspot.com",
+    messagingSenderId: "469622956981"
+};
